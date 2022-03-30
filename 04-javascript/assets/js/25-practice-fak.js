fe762c5184d5e26b4539e93365ece95a3af97681
okurturgay3902 <94409068+okurturgay3902@users.noreply.github.com>
2022-03-30 22:19:36 +0200

    scobe

---
+++ 04-javascript/assets/js/25-practice-fak.js
@@ -0,0 +1,17 @@
+const hesapla = () => {
+    const sayi = document.querySelector("#txtSayi").value;
+
+    if(!sayi || isNaN(sayi) || sayi < 0) {
+        alert("Pozitif bir tam sayi giriniz");
+        return;
+    }
+
+    let carpim=1;
+
+    for(let i = sayi; i>0; i--) {
+        carpim = carpim*i;
+
+}
+
+document.querySelector("#sonuc").innerHTML=carpim;
+}
